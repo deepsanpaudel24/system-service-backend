2e5e63b3d85c17f9656f0e96c888a387dbd7eac1
Roshan Pratap Katel <paradise98425@gmail.com>
2020-08-31 22:15:05 +0545

    file updated

--- app/resources/landingpage.py
+++ app/resources/landingpage.py
@@ -2,4 +2,4 @@ from flask_restful import Resource
 
 class LandingPage(Resource):
     def get(self):
-        return {"message": "Welcome to Landing Page!"}
+        return {"message": "Welcome to Landing Page! Please Sign up to continue"}
